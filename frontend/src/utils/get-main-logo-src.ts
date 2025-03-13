7e685d4f23485d2c08aec41b9e7a7530600d0f2a
anicsonrico <anicsonrico@gmail.com>
2025-03-13 14:04:11 +0100

    Frontend getmainlogo

--- frontend/src/utils/get-main-logo-src.ts
+++ frontend/src/utils/get-main-logo-src.ts
@@ -1,11 +1,5 @@
 import { Theme } from "~/styles/themes"
+import { CONSTANTS } from "./constants"
 
-import { LOGO_FULL_DARK_SRC, LOGO_FULL_SRC } from "./constants"
-
-export const getMainLogoSrc = (theme?: string) => {
-  if (theme === Theme.Dark) {
-    return LOGO_FULL_DARK_SRC
-  }
-
-  return LOGO_FULL_SRC
-}
+export const getMainLogoSrc = (theme?: string): string =>
+  theme === Theme.Dark ? CONSTANTS.LOGO_FULL_DARK_SRC : CONSTANTS.LOGO_FULL_SRC
